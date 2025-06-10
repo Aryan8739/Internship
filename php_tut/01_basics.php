eae80bd7886769e2b7571bb8660c1dbe519ab4a1
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-10 10:34:30 +0000

    syntax

---
+++ php_tut/01_basics.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tutorial</title>
+</head>
+<body>
+    <div class="container">
+        This is my first php website.
+        <?php
+        echo "Hello World and this is printied in PHP...!!";
+        ?>
+        <?php
+        echo "Hello World Again.";
+
+
+        $var1 = 34;
+        $var2 = 35;
+        echo $var1+ $var2;
+        ?>
+    </div>
+    
+</body>
+</html>
