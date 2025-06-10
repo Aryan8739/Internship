b72bf0728640a488b4b1d267daa678909be79cf8
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-10 16:40:16 +0530

    Add files via upload

---
+++ php_tut/04_moreBasics.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tut>
+</head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .container{
+        max-width: 80%;
+        background-color: red;
+        margin: auto;
+    }
+</style>
+<body>
+<div class="container">
+    This is a container...
+    <?php
+
+
+    ?>
+</div>
+    
+</body>
+</html>
