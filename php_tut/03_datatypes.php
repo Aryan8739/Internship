7b1df1c780af8ef2ab97424b8aecc1086b878501
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-10 16:31:58 +0530

    datatypes

---
+++ php_tut/03_datatypes.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    define("g",9.8);
+
+    // Datatypes
+    /*
+    String
+    Integer
+    float
+    boolean
+    Array
+    Object
+    */
+
+    $var = "This is a String";
+    var_dump($var);
+    echo "<br>";
+
+    $var = 69;   
+    var_dump($var);
+    echo "<br>";
+
+    $var = 69.1; 
+    var_dump($var);
+    echo "<br>";
+
+    $var = false;
+    var_dump($var);
+    echo "<br>";
+    
+    echo "<br>";
+    echo g;
+   ?>
+
+    
+</body>
+</html>
