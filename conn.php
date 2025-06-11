f50174047e0cac664567fd43bbe9b8285f88f9a9
Aryan <aryan.rastogi.20nov@gmail.com>
2025-06-11 16:48:52 +0530

    new conn

--- conn.php
+++ conn.php
@@ -1,12 +1,16 @@
 <?php 
-$username = "root"; 
-$host = "localhost" ;
-$password = "" ;
-$base = "project" ;
 
-$conn = new mysqli($username,$host,$password,$base);
+$host = "localhost";
+$username = "root";
+$pass = "";
+$DB_name = "project";
 
+$port =  "3307";
 
-
-
-?>
+$conn = new mysqli($host , $username, $pass, $DB_name , $port);
+if (!$conn) {
+    die("COnnection not established");
+} else{
+    // echo "connected";
+    // die;
+}
