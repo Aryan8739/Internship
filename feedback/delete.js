d9db665b6fbadb55fe2af3cd1dd60633c26e2a3d
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2025-06-16 14:22:13 +0530

    delete

--- feedback/delete.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("delete-form").addEventListener("submit", function (e) {
-  const confirmation = confirm("Are you sure you want to delete this record?");
-  if (!confirmation) {
-    e.preventDefault();
-  }
-});
-
