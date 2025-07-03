fe6a861114ac3ddeeb51559bf01c00e3907eaddb
Aryan Rastogi <aryan.rastogi.20nov@gmail.com>
2025-07-03 17:09:17 +0530

    session changes

--- Project01/Admin/admin_logout.php
+++ Project01/Admin/admin_logout.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
+
+// Unset all admin-related session variables
 unset($_SESSION['admin_id']);
 unset($_SESSION['admin_name']);
 unset($_SESSION['is_admin']);
-header("Location: login.php");
+
+// Optionally clear all session variables
+// session_unset();  // Uncomment if you want to clear ALL session data
+
+// Destroy the session if needed
+session_destroy(); // Uncomment if you want to destroy session entirely
+
+// Redirect to login page
+header("Location: ../login.php");
 exit;
+?>
